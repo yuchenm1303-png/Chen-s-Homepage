7486ccaedf2d32109529e70d12ed6fdd2324dd2d
yuchenm1303-png <yuchenm1303@gmail.com>
2026-08-20 16:31:48 +0800

    wire public website copy into service monitor

--- download/usage-admin-v2.js
+++ download/usage-admin-v2.js
@@ -1,3 +1,4 @@
+import "./usage-public-copy-v1.js?v=20260820-1630";
 import "./usage-copy-v1.js?v=20260819-1005";
 import "./usage-admin-v2-core.js?v=20260820-1600";
 import "./usage-chart-tooltip-v1.js?v=20260819-1134";

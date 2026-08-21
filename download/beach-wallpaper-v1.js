4e254b20f850073b5ead39973ed5411451423e2f
yuchenm1303-png <yuchenm1303@gmail.com>
2026-08-21 10:35:26 +0800

    fix: remove backdrop root blocking card blur

--- download/beach-wallpaper-v1.js
+++ download/beach-wallpaper-v1.js
@@ -1,5 +1,5 @@
 (() => {
-  const ASSET_VERSION = "20260821-1210";
+  const ASSET_VERSION = "20260821-1245";
   const STYLE_ID = "beach-wallpaper-v1-style";
   const STYLE_URL = `./beach-wallpaper-v1.css?v=${ASSET_VERSION}`;
 
